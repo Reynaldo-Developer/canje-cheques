8cd84e3532c2869d541f6104e85a9cdfc72fc14a
ReynaldoYovera <reynaldo.yovera@gmail.com>
2026-02-06 18:23:25 -0500

    primer proyecto

---
+++ src/main/java/com/canjecheques/canje_cheques/CanjeChequesApplication.java
@@ -0,0 +1,13 @@
+package com.canjecheques.canje_cheques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanjeChequesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanjeChequesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canjecheques/canje_cheques/CanjeChequesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canjecheques.canje_cheques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanjeChequesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
